216bded9c5a1553526967fc557f831719f2b000f
internetabuse <167064173+internetabuse@users.noreply.github.com>
2024-04-16 13:22:36 +0400

    Add files via upload

---
+++ sauce.cy.js
@@ -0,0 +1,112 @@
+describe("Регистрация нового пользователя", () => {
+    it("Успешная регистрация", () => {
+    cy.visit("https://petstore.swagger.io");
+    cy.contains("New User?").click();
+
+
+    
+    cy.get("#firstName").type("John");
+    cy.get("#lastName").type("Doe");
+    cy.get("#username").type("johndoe");
+    cy.get("#password").type("password");
+
+
+        
+    cy.get("#register-button").click();
+
+
+        
+    cy.contains("Registered Successfully!");
+    });
+    });
+  
+    describe("Авторизация существующего пользователя", () => {
+      it("Успешная авторизация", () => {
+      cy.visit("https://petstore.swagger.io");
+
+          
+      cy.get("#user-name").type("standard_user");
+      cy.get("#password").type("secret_sauce");
+
+          
+      cy.get("#login-button").click();
+
+          
+      cy.contains("PRODUCTS");
+      });
+      });
+  
+      describe("Добавление товара в корзину", () => {
+        it("Товар успешно добавлен в корзину", () => {
+        cy.visit("https://petstore.swagger.io");
+
+            
+        cy.get("#user-name").type("standard_user");
+        cy.get("#password").type("secret_sauce");
+
+            
+        cy.get("#login-button").click();
+
+            
+        cy.get(".inventory_item")
+      });
+    });
+  
+  
+  describe('Поиск товара', () => {
+    beforeEach(() => {
+    cy.visit('https://petstore.swagger.io')
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.url().should('include', '/inventory.html')
+    })
+    
+    it('Поиск существующего товара', () => {
+    cy.get('.inventory_filter_container .product_sort_container').select('za')
+    cy.get('.inventory_item_container .inventory_item:nth-child(1)').contains('Sauce Labs Onesie').should('exist')
+    })
+    
+    it('Поиск несуществующего товара', () => {
+    cy.get('.inventory_filter_container .product_sort_container').select('za')
+    cy.get('.inventory_item_container .inventory_item:nth-child(1)').contains('Nonexistent Item').should('not.exist')
+    })
+    })
+  
+    describe('Сортировка товаров', () => {
+      beforeEach(() => {
+      cy.visit('https://petstore.swagger.io')
+      cy.get('#user-name').type('standard_user')
+      cy.get('#password').type('secret_sauce')
+      cy.get('#login-button').click()
+      cy.url().should('include', '/inventory.html')
+      })
+      
+      it('Сортировка по цене (от высокой к низкой)', () => {
+      cy.get('.inventory_filter_container .product_sort_container').select('hilo')
+      cy.get('.inventory_item_container .inventory_item:nth-child(1)').contains('Sauce Labs Fleece Jacket').should('exist')
+      })
+      
+      it('Сортировка по цене (от низкой к высокой)', () => {
+      cy.get('.inventory_filter_container .product_sort_container').select('lohi')
+      cy.get('.inventory_item_container .inventory_item:nth-child(1)').contains('Sauce Labs Backpack').should('exist')
+      })
+      })
+  
+      describe('Оформление заказа', () => {
+        beforeEach(() => {
+        cy.visit('https://petstore.swagger.io')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+        cy.url().should('include', '/inventory.html')
+        })
+        
+        it('Добавление товара в корзину и переход к оформлению заказа', () => {
+        cy.get('.inventory_item:nth-child(1) .btn_inventory').click()
+        cy.get('.shopping_cart_link').click()
+        cy.url().should('include', '/cart.html')
+        cy.get('.checkout_button').click()
+        cy.url()
+      })
+    })
